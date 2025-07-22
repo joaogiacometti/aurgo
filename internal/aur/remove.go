368a893b0372caac5e44654a9495c78afad8e7b0
Joao Pedro Giacometti Guerreiro <j.guerreiro@unesp.br>
2025-07-22 15:35:50 -0300

    fix(options): stop trying to remove clone folder when uninstalling
    The clone folder is removed after installing, there's not cache anymore

--- internal/aur/remove.go
+++ internal/aur/remove.go
@@ -3,26 +3,11 @@ package aur
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 
-	"github.com/joaogiacometti/aurgo/internal/config"
 	"github.com/joaogiacometti/aurgo/internal/helpers"
 )
 
 func RemovePackage(pkgName string) error {
-	packagePath := filepath.Join(config.DataDir, pkgName)
-
-	ok, err := helpers.IsDir(packagePath)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return fmt.Errorf("package '%s' not found in cache", pkgName)
-		}
-		return fmt.Errorf("failed to stat package path '%s': %w", packagePath, err)
-	}
-	if !ok {
-		return fmt.Errorf("'%s' is not a directory", packagePath)
-	}
-
 	if err := helpers.RemoveWithPacman(pkgName); err != nil {
 		return fmt.Errorf("pacman failed to remove package '%s': %w", pkgName, err)
 	}
